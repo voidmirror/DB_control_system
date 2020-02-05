dbf7622c2efe82a8be057ab52e5b90707ea84b9d
Voidmirror <voidmirror@users.noreply.github.com>
2020-02-05 15:47:13 +0400

    TextField ID

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,12 +2,14 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
 
 public class Controller {
     @FXML
     private Tab tabCreateNewUser;
     @FXML
     private Tab tabDataBase;
-
+    @FXML
+    private TextField textFieldUserName;
 
 }
