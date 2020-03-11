fae78187daed0ab5153c7e40fe6a0586bc188f06
Voidmirror <voidmirror@users.noreply.github.com>
2020-03-11 14:24:43 +0400

    Change Tabs

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,17 +3,25 @@ package sample;
 import javafx.animation.FadeTransition;
 import javafx.animation.PauseTransition;
 import javafx.animation.SequentialTransition;
+import javafx.beans.InvalidationListener;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.util.Duration;
 
 import java.io.*;
 import java.net.URI;
 import java.nio.file.*;
-
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 
 public class Controller {
@@ -42,6 +50,188 @@ public class Controller {
     @FXML
     private Label labelLog;
 
+    // TableView
+    @FXML
+    private TableView tableViewUsers;
+    @FXML
+    private TableColumn columnUserName;
+    @FXML
+    private TableColumn columnPassword;
+    @FXML
+    private TableColumn columnEmail;
+    @FXML
+    private TableColumn columnBlackMatter;
+    @FXML
+    private TableColumn columnUserLevel;
+    @FXML
+    private TableColumn columnVIP;
+
+    private ObservableList<UserAccount> usersData = new ObservableList<UserAccount>() {
+        @Override
+        public void addListener(ListChangeListener<? super UserAccount> listChangeListener) {
+
+        }
+
+        @Override
+        public void removeListener(ListChangeListener<? super UserAccount> listChangeListener) {
+
+        }
+
+        @Override
+        public boolean addAll(UserAccount... userAccounts) {
+            return false;
+        }
+
+        @Override
+        public boolean setAll(UserAccount... userAccounts) {
+            return false;
+        }
+
+        @Override
+        public boolean setAll(Collection<? extends UserAccount> collection) {
+            return false;
+        }
+
+        @Override
+        public boolean removeAll(UserAccount... userAccounts) {
+            return false;
+        }
+
+        @Override
+        public boolean retainAll(UserAccount... userAccounts) {
+            return false;
+        }
+
+        @Override
+        public void remove(int i, int i1) {
+
+        }
+
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean contains(Object o) {
+            return false;
+        }
+
+        @Override
+        public Iterator<UserAccount> iterator() {
+            return null;
+        }
+
+        @Override
+        public Object[] toArray() {
+            return new Object[0];
+        }
+
+        @Override
+        public <T> T[] toArray(T[] ts) {
+            return null;
+        }
+
+        @Override
+        public boolean add(UserAccount account) {
+            return false;
+        }
+
+        @Override
+        public boolean remove(Object o) {
+            return false;
+        }
+
+        @Override
+        public boolean containsAll(Collection<?> collection) {
+            return false;
+        }
+
+        @Override
+        public boolean addAll(Collection<? extends UserAccount> collection) {
+            return false;
+        }
+
+        @Override
+        public boolean addAll(int i, Collection<? extends UserAccount> collection) {
+            return false;
+        }
+
+        @Override
+        public boolean removeAll(Collection<?> collection) {
+            return false;
+        }
+
+        @Override
+        public boolean retainAll(Collection<?> collection) {
+            return false;
+        }
+
+        @Override
+        public void clear() {
+
+        }
+
+        @Override
+        public UserAccount get(int i) {
+            return null;
+        }
+
+        @Override
+        public UserAccount set(int i, UserAccount account) {
+            return null;
+        }
+
+        @Override
+        public void add(int i, UserAccount account) {
+
+        }
+
+        @Override
+        public UserAccount remove(int i) {
+            return null;
+        }
+
+        @Override
+        public int indexOf(Object o) {
+            return 0;
+        }
+
+        @Override
+        public int lastIndexOf(Object o) {
+            return 0;
+        }
+
+        @Override
+        public ListIterator<UserAccount> listIterator() {
+            return null;
+        }
+
+        @Override
+        public ListIterator<UserAccount> listIterator(int i) {
+            return null;
+        }
+
+        @Override
+        public List<UserAccount> subList(int i, int i1) {
+            return null;
+        }
+
+        @Override
+        public void addListener(InvalidationListener invalidationListener) {
+
+        }
+
+        @Override
+        public void removeListener(InvalidationListener invalidationListener) {
+
+        }
+    };
 
     public String getTextFieldUserName() {
         return textFieldUserName.getText();
@@ -157,5 +347,12 @@ public class Controller {
 
         //FileOutputStream outputStream = new FileOutputStream(dataBase, true);
         //FileWriter fileWriter = new FileWriter("src/database.txt", true);
+    }
+
+    public void initializeTableView() {
+        //columnUserName.setCellValueFactory(new PropertyValueFactory<>());
+        for (int i = 0; i < Main.accountList.getDataBaseHashMap().size(); i++) {
+            //TODO: usersData.add
+        }
     }
 }
